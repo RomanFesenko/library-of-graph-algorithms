d902cfa42bb7c3b35c6fe8bac4c0601d32816309
Roman Fesenko <72913560+RomanFesenko@users.noreply.github.com>
2024-01-17 08:58:51 +0200

    main
    main

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <assert.h>
+
+#include "test.h"
+#include "matching/test.h"
+#include "leetcode_graph_problem/test.h"
+
+
+int main()
+{
+    gtl_test();
+    test_matching();
+    test_leetcode();
+    assert(false);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
